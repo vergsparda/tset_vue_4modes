ae825389f748face52f0aa259f1ee994ea5b06d7
Alex Smith <alex.vesson.smith@gmail.com>
2021-04-25 18:03:37 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1 class="tittle">modes</h1>
+    <buttons/>
+    <loader/>
+  </div>
+</template>
+
+<script>
+import buttons from "@/components/buttons";
+import loader from "@comonents/loader";
+
+export default {
+  name: 'App',
+  components: {
+    buttons, loader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family:Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html {
+  width: 100%;
+  height: 100%;
+  }
+
+</style>
---
+++ src/components/buttons.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="btn">
+    <button class="btn-red">1</button>
+    <button class="btn-green">2</button>
+    <button class="btn-blue">3</button>
+    <button class="btn-yellow">4</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+}
+  // const btnRed = document.querySelector('.btn-red');
+  // // const btnGreen = document.querySelector('.btn-green');
+  // // const btnBlue = document.querySelector('.btn-blue');
+  // // const btnYellow = document.querySelector('.btn-yellow');
+  // const html = document.querySelector("html");
+
+  // html.addEventListener('click', (e) => {
+  //   if (e === btnRed) {
+  //   e.preventDefault();
+  //   html.classList.toogle("btn-red");
+  //   }
+  // })
+
+</script>
+
+<style scoped>
+  button {
+  box-sizing: border-box;
+  width: 100px;
+  height: 30px;
+}
+
+.btn-red {
+  background-color: red;
+}
+
+.btn-green {
+  background-color: green;
+}
+.btn-blue {
+  background-color: blue;
+}
+
+.btn-yellow {
+  background-color: yellow;
+}
+</style>
+
